29d33ec0dde112c20303eb80b748441630fd1a52
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-07 13:24:55 +0100

    2nd commit main

--- js/app.js
+++ js/app.js
@@ -1,3 +1,35 @@
+
+// Events..
+
+// Requires three steps..
+
+// [ 1. ] Which element is dispatching the event..
+
+// [ 2. ] What is the event..
+
+// [ 3. ] What code should be extended..
+
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript has loaded');
+
+  const button = document.querySelector('#button');
+  button.addEventListener("click", handleButtonClick);
+
+  const textInput = document.querySelector("#input");
+  textInput.addEventListener('input', handleInput)
+
 });
+
+const handleButtonClick = function (){
+  // console.log('Button has been clicked');
+  const resultParagraph = document.querySelector('#button-result');
+  resultParagraph.textContent = 'That button has definitley been clicked';
+}
+
+const handleInput = function (){
+  // console.log('Input has been typed in');
+  // 1. Get value from the input..
+  // 2. Update the paragraph text..
+  
+}
